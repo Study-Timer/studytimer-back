83ee7258efc5678eb1e6654de32185abae343267
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2021-07-19 20:15:53 -0300

    changes

--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ const express = require('express')
 
 require('./database')
 
+const PORT = process.env.PORT || 3000
 
 const app = express()
+app.use(express.json())
+// app.use(routes)
 
-const PORT = process.env.PORT || 3000
 
 
 app.listen(() => {
