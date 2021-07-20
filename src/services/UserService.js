e44f239393e308e8c62382311cc32e35fced07fb
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2021-07-19 21:12:17 -0300

    feat: adds user service

---
+++ src/services/UserService.js
@@ -0,0 +1,15 @@
+// import model
+
+const getUser = async (id) => {}
+
+const createUser = async (username, email, password) => {}
+
+const updateUser = async (data) => {} 
+
+
+
+module.exports = {
+    getUser,
+    createUser, 
+    updateUser
+}
