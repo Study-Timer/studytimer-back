3a5b91c66a42e3b0ba898c6dd80823c946cdc059
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2021-07-19 21:47:57 -0300

    fix: number port

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
 const express = require('express')
+const routes = require('./routes.js')
 
 require('./database')
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3333
 
 const app = express()
 app.use(express.json())
-// app.use(routes)
+app.use(routes)
 
 
 
-app.listen(() => {
+app.listen(PORT, () => {
     console.log(`App listening on port: ${PORT}`)
 })
