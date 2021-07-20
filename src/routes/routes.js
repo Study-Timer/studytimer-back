cf939bf62504361b05cf9e68100a3bab0f2b0c78
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2021-07-19 21:04:12 -0300

    feat: adds routes for entities

---
+++ src/routes/routes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+// const UserController
+
+const routes = express.Router()
+
+
+// routes.get('/users', UserController.index)
+
+
+
+
+
+module.exports = routes
