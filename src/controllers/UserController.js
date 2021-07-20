0e574e508af92e575155cef78a63920d59eab875
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2021-07-19 21:04:25 -0300

    feat: adds user controller

---
+++ src/controllers/UserController.js
@@ -0,0 +1,17 @@
+module.exports = {
+    async index(req, res) {
+
+    },
+
+    async indexOne(req, res) {
+
+    }, 
+
+    async store(req, res) {
+
+    }, 
+
+    async destroy(req, res) {
+
+    }
+}
