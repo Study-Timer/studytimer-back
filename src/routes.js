0448c70bcdfdfd367200a0df98cfb6834c868fb3
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2021-08-04 20:07:30 -0300

    feat: adds auth in update user route

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,7 @@ routes.get('/', UserController.ping)
 
 routes.post('/login', UserController.login)
 routes.post('/users', UserController.store)
-routes.post('/users/:user_id', UserController.updateUser)
+routes.post('/users/:user_id', UserController.verifyJWT, UserController.updateUser)
 routes.get('/users/:user_id', UserController.verifyJWT ,UserController.indexOne)
 
 
